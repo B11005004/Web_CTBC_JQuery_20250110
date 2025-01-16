8e3009e073ad0d1b0d9dfbbf9a4a9dc86708ac15
B11005004 <b11005004@ctbc.edu.tw>
2025-01-16 12:18:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ $("#show-2").click(()=> {
         $("#h3-2").text("我是標題三顯示");
     });
 });
+
+// 3.滑入滑出效果
+const h3_3 = $("#h3-3");
+
+$("#slideUp-3").click(()=>{
+    h3_3.slideUp(1000);
+});
+
+$("#slideDown-3").click(()=>{
+    h3_3.slideDown(1000);
+});
+
+// 4.css函式應用
+$("#btn-4").click(()=>{
+    $("#box-4").addClass("btn");
+});
+
+// dblclick特別注意
+$("#box-4").dblclick(()=>{
+    $("#box-4").text("我是箱子被點兩下");
+});
